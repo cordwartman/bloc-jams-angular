0cd41c92b9ebef7fa9f49cad3d8f8227fa7fe835
Cord Wartman <cordwartman@gmail.com>
2016-11-23 13:42:05 -0500

    set the seek bar to update as songs play

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
         /**
         * @desc public function library
         * @type {Object}
@@ -29,6 +29,13 @@
                 formats: ['mp3'],
                 preload: true   
             });
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function(){
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+            
             SongPlayer.currentSong = song;
         };
         /**
@@ -63,6 +70,11 @@
         * @type {Object}
         */
         SongPlayer.currentSong = null;
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
         /**
         * @function play public function
         * @desc Plays the song and sets it to current if not the current song
@@ -123,9 +135,20 @@
             }
         };
         
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+        
         return SongPlayer;
     }
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
